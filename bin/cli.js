fa5391ebb94208404e32d1e5d4db1411e9caa34d
javarouka <javarouka@coupang.com>
2020-02-09 18:05:10 +0900

    support npx

--- bin/cli.js
+++ bin/cli.js
@@ -16,12 +16,36 @@ const destination = args[0];
 const destinationTemp = `${destination}_temp`;
 const srcDir = `${destinationTemp}/boilerplate`;
 
+console.log(`> 🚚 The file is being cloned to a temporary directory > [${destinationTemp}]`)
+
+function checkExeResult(code, stdout, stderr) {
+    if(code !== 0) {
+        console.error(stderr);
+        process.exit(code);
+    }
+    return true;
+}
+
+function processMoveBoilerplateFile(code, stdout, stderr) {
+    checkExeResult(code, stdout, stderr)
+    console.log('> 🚚 complete cloning.')
+    console.log(`> 📦 start moving boilerplate files to [${destination}] directory`)
+    shell.exec(`mv ${srcDir} ${destination}/`, processDeleteTempDirectory);
+}
+function processDeleteTempDirectory(code, stdout, stderr) {
+    checkExeResult(code, stdout, stderr)
+    console.log(`> 📦 moved boilerplate files to [${destination}] directory`)
+    console.log(`> 🗑️  start cleansing job ...`)
+    shell.exec(`rm -rf ${destinationTemp}`, processCleansing);
+}
+function processCleansing(code, stdout, stderr) {
+    checkExeResult(code, stdout, stderr)
+    console.log(`> 🗑️  completed cleansing job`)
+    console.log(`> 😃 enjoy development!`)
+}
+
 shell.exec(
     'git clone https://github.com/javarouka/minimal-electron-typescript-boilerplate.git ' + destinationTemp,
-    function() {
-        console.log("complete cloning")
-        shell.exec(`mv ${srcDir} ${destination}/`);
-        shell.exec("rm -rf " + destinationTemp);
-    }
+    processMoveBoilerplateFile
 )
 
